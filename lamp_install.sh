4bf09034fab6d3b4262e94c0dcb341bd1e061e1d
Ramon Domingos Duarte Oliveira <ramon@digisaude.com>
2018-06-26 14:22:34 -0300

    adicao da pasta do phpmyadmin para o www

--- lamp_install.sh
+++ lamp_install.sh
@@ -4,5 +4,6 @@ sudo apt-get install apache2 mysql-server php php-mysql libapache2-mod-php
 sudo apt-get install phpmyadmin
 sudo apt-get install php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
 echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/test.php
+sudo cp-r /usr/share/phpmyadmin/ /var/www/html/
 sudo /etc/init.d/apache2 restart
-echo "Agora vai la no navegador http://localhost/test.php e veja se funcionou :)"
+echo "Agora vai la no navegador http://localhost/test.php e http://localhost/phpmyadmin/ e veja se funcionou :)"
