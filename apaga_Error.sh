4b0ae8abcf7d23e5c0b3174693350b2da15affd3
Ramon Domingos Duarte Oliveira <ramon@digisaude.com>
2018-06-26 15:24:40 -0300

    ajuste nos parametros, para aceitar nenhum e apagar a pasta raiz

--- apaga_Error.sh
+++ apaga_Error.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-rm  /var/www/html/$1/error_log.txt
-touch /var/www/html/$1/error_log.txt
+if [ "$1" = "" ]; then
+  rm  /var/www/html/error_log.txt
+  touch /var/www/html/error_log.txt
+else
+  rm  /var/www/html/$q/error_log.txt
+  touch /var/www/html/$1/error_log.txt
+fi
