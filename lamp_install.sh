65fb2d976389e16fd7d3367e78cdca1a478ed6b1
Ramon Domingos Duarte Oliveira <ramon@digisaude.com>
2018-06-26 13:30:33 -0300

    script de instalacao Apache, MySQL e PHP no ubuntu

---
+++ lamp_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install lamp-server^
+sudo apt-get install apache2 mysql-server php php-mysql libapache2-mod-php
+sudo apt-get install phpmyadmin
+sudo apt-get install php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
+echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/test.php
+sudo /etc/init.d/apache2 restart
+echo "Agora vai la no navegador http://localhost/test.php e veja se funcionou :)"
